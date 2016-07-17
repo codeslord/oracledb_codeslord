03f5b3e030e2bf0075aaeca19e75d2724b98dd13
codeslord <rohithrnair@outlook.com>
2016-07-17 23:55:20 +0530

    Update session_basic.sql

--- session_basic.sql
+++ session_basic.sql
@@ -1,1 +1,5 @@
-select sid,serial#,username,osuser,status,state,machine from v$session;
+set lines 200 pages 1000
+col osuser for a15
+col machine for a30
+col username for a20
+select sid,serial#,username,machine,terminal,osuser,state,status,sql_id from v$session;
