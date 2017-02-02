3fba4411f3b786fc4803abb02e3460ed1b245f76
codeslord <rohithrnair@outlook.com>
2017-02-02 15:45:30 +0530

    Update dbf_size.sql

--- dbf_size.sql
+++ dbf_size.sql
@@ -1,2 +1,5 @@
 select sum(bytes/1024/1024) Total_MB from dba_data_files where file_id=&fileid;
 select sum(bytes/1024/1024) Free_MB from dba_free_space where file_id=&fileid;
+
+SELECT FILE#,name, bytes/1024/1024 AS size_mb
+FROM   v$datafile;
