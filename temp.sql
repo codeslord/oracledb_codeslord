a74aae9400bf230dfc534626db0fd0b8324ee84f
codeslord <rohithrnair@outlook.com>
2017-03-21 19:40:28 +0530

    Update temp.sql

--- temp.sql
+++ temp.sql
@@ -245,3 +245,10 @@ FROM v$session a, v$tempseg_usage b, v$sqlarea c,
     AND c.hash_value = a.sql_hash_value
     AND (b.blocks*d.block_size)/1048576 > 1024
     ORDER BY b.tablespace, 6 desc;
+    
+    
+SELECT	A.tablespace_name tablespace, D.mb_total,SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_used, D.mb_total - SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_free
+FROM	v$sort_segment A,(SELECT B.name, C.block_size, SUM (C.bytes) / 1024 / 1024 mb_total
+FROM	v$tablespace B, v$tempfile C
+WHERE	B.ts#= C.ts# GROUP BY B.name, C.block_size)D WHERE A.tablespace_name = D.name
+GROUP	By A.tablespace_name, D.mb_total;
